836eda53065d102286b071e91bf85dafef53917b
paimpozhil <paimpozhil@gmail.com>
2014-05-17 17:21:27 -0400

    start script updated with more options

--- stdcontainers/startall.sh
+++ stdcontainers/startall.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
-appname=$1
 
-docker run -td --name $appname dp-data 
+while getopts ":a:dhnup" opt; do
+  case $opt in	
+     a)
+        appname=$OPTARG
+        docker run -td --name $appname dp-data
+        ;;
 
-docker run --volumes-from $appname --name $appname-mysql  -p 3306:3306  -d stdmysql /sbin/my_init 
+    d)
+    docker run --volumes-from $appname --name $appname-mysql  -p 3306  -d stdmysql /sbin/my_init
+    mylink=" --link $appname-mysql:mysqldb"
+	;;
+    p)
+    docker run  -e VIRTUAL_HOST=$appname $mylink --name $appname-apache  --volumes-from $appname -p 80  -d stdapache
+    apachelink=" --link $appname-apache:apache"
+ 	;;
+    n)
+    docker run  -e VIRTUAL_HOST=$appname $mylink --name $appname-nginx  --volumes-from $appname -p 80  -d stdnginx /sbin/my_init
+    nginxlink=" --link $appname-nginx:nginx"
+ 	;;
+    u)
+    docker run $mylink   $apachelink $nginxlink --name $appname-utils --volumes-from $appname -p 22  -d stdutils /sbin/my_init
+                   ;;
+    h)
+    echo "startall.sh -a appname -n {fornginx} -d {formysql} -p {forapache} -u {forutils}"
+    echo "sample usage ./startall.sh yourwebsite.com -n -d -u"
+		;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+	
+      ;;
+  esac
+done
 
-docker run  --link $appname-mysql:mysqldb --name $appname-nginx  --volumes-from $appname -p 80:80  -d stdnginx /sbin/my_init 
-
-docker run  --link $appname-mysql:mysqldb --name $appname-apache  --volumes-from $appname -p 81:80  -d stdapache 
-
-docker run  --link $appname-mysql:mysqldb --link $appname-nginx:nginx --link $appname-apache:apache  --name $appname-utils --volumes-from $appname -p 2222:22  -d stdutils /sbin/my_init
-
-
-echo " $appname - created "
+#echo " $appname - created "
