16cc5d09ba8e3124500e1ad45b24bf930a4577c2
paimpozhil <paimpozhil@gmail.com>
2014-05-26 15:14:24 -0400

    Selectively build the images

--- stdcontainers/buildall.sh
+++ stdcontainers/buildall.sh
@@ -1,17 +1,47 @@
 #! /bin/bash
 
-mkdir keys
+while getopts ":k:dhnupmlx" opt; do
+  case $opt in	
 
-ssh-keygen -f keys/dpanel-key-$1
+    k)
+	keyname=$OPTARG
 
-DIRS=`find  -mindepth 1 -maxdepth 1 -type d -exec basename {} \;`
+	ssh-keygen -f keys/$keyname
 
-for dir in $DIRS; do cp keys/dpanel-key-$1.pub "$dir/dpanel_ssh_key.pub"; done
+	DIRS=`find  -mindepth 1 -maxdepth 1 -type d -exec basename {} \;`
 
-cp -f keys/dpanel-key-$1 utils/dpanel_ssh_key
+	for dir in $DIRS; do cp keys/$keyname.pub "$dir/dpanel_ssh_key.pub"; done
 
-for dir in $DIRS; do
+	cp -f keys/$keyname utils/dpanel_ssh_key
+	;;
 
-docker build -t img$dir $dir/.
+    d)
+	docker build -t imgmysql mysql/.
+	;;
+    p)
+	docker build -t imgapachephp53 apachephp53/.
+	;;
+    n)
+        docker build -t imgnginx nginx/.
+ 	;;
+    x)
+	docker build -t imgnginxphp53 nginxphp53/.
+	;;
+    l)
+	docker build -t imglamp lamp/.
+                  ;;
+    u)
+	docker build -t imgutils utils/.
+                   ;;
+    
+    h)
+    echo "buildall.sh -k keyname -x {fornginx php53} -d {formysql} -p {forapache php53} -u {forutils} -n {for nginx php55 } -l {for apachephp53 plus mysql}"
+    echo "sample usage ./buildall.sh -k mykey -n -d -u or ./buildalll.sh -k mykey -l -u"
+		;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
 
+      ;;
+  esac
 done
+
